bd590eb82519acf79901e9fd553be2d263306a33
raulgomez <raul_eje@hotmail.com>
2020-06-03 16:37:56 +0200

    marvellous

---
+++ vuesterslayer/vuesterslayer/src/components/bars.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="hpbars">
+      <img src="../assets/ryu.jpeg" alt="ryu" />
+      <p class="name">Player</p>
+      <div class="red">
+        <div class="bar" :style="{ width: playerHp + '%' }"></div>
+      </div>
+      <img src="../assets/bison.jpeg" alt="bison" />
+      <p class="name">Enemy</p>
+      <div class="red">
+        <div class="bar" :style="{ width: enemyHp + '%' }"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "lifebar",
+  props: {
+    playerHp: Number,
+    enemyHp: Number
+  }
+};
+</script>
+<style scoped>
+img {
+  width: 5rem;
+}
+.hpbars {
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  background-color: rgba(255, 255, 255, 0);
+}
+.red {
+  background: red;
+}
+.bar {
+  background: yellow;
+  height: 25px;
+}
+.name {
+  width: 10rem;
+}
+</style>
